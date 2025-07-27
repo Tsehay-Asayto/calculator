1cb960bafec04c9fde36441b320e9514878252c8
Tsehay Asayto <tsehayasayto9@gmail.com>
2025-07-27 13:19:57 -0700

    third commit

--- calculator.py
+++ calculator.py
@@ -2,4 +2,16 @@ n=float(input("Enter the first number: "))
 m=float(input("Enter the second number: "))
 r=input("Enter the operation: ")
 if  r=="+":
-    print(n+m)
+    result=n+m
+elif r=="-":
+    result=n-m
+elif r=="*":
+    result=n*m
+elif r=="/":
+    if m==0:
+        print("Zero division error")
+    else:
+     result=n/m
+else:
+    print("Please enter valid data")
+print(n ,r, m,"=",result)
